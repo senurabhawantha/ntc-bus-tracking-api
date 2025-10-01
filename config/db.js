5381a6fe1fe3d90a44329dca05badef12a951c9e
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-01 16:34:04 +0530

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// config/db.js
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env file
+
+const mongoUri = process.env.MONGO_URI || 'mongodb+srv://senurabawantha_db_user:koOuQbtRbIJcI4sd@bus-traking-api.hxctmjs.mongodb.net/?retryWrites=true&w=majority&appName=bus-traking-api'; // Default to local DB if not set
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected successfully');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1); // Exit the process if DB connection fails
+  }
+};
+
+module.exports = connectDB;

96f392261ac72ddd84a5ea596d79a0716f7a1516
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-01 16:32:04 +0530

    Create route.js

---
+++ models/route.js
@@ -0,0 +1,18 @@
+// models/route.js
+const mongoose = require('mongoose');
+
+const routeSchema = new mongoose.Schema({
+  route_id: { 
+    type: Number, 
+    required: true, 
+    unique: true 
+  },
+  route_name: { 
+    type: String, 
+    required: true 
+  }
+});
+
+const Route = mongoose.model('Route', routeSchema);
+
+module.exports = Route;

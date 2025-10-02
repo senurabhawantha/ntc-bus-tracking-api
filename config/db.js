a3506793d1cae435216fcb44dbe14f4c55ed6a39
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-02 14:45:58 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,15 +5,45 @@ const mongoUri = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoUri);
+    await mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
-    process.exit(1);
+    process.exit(1); // Exit the process if DB connection fails
   }
 };
 
 module.exports = connectDB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const mongoUri = process.env.MONGO_URI;
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(mongoUri);
+//     console.log('MongoDB connected successfully');
+//   } catch (err) {
+//     console.error('Error connecting to MongoDB:', err);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 
 
 

318f503cac6650a5271cba0e950162dae3f1cdf1
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-02 12:39:42 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,50 @@
-// config/db.js
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables from .env file
+require('dotenv').config();
 
-const mongoUri = process.env.MONGO_URI || 'mongodb+srv://senurabawantha_db_user:koOuQbtRbIJcI4sd@bus-traking-api.hxctmjs.mongodb.net/?retryWrites=true&w=majority&appName=bus-traking-api'; // Default to local DB if not set
+const mongoUri = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoUri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(mongoUri);
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error('Error connecting to MongoDB:', err);
-    process.exit(1); // Exit the process if DB connection fails
+    process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // config/db.js
+// const mongoose = require('mongoose');
+// require('dotenv').config(); // Load environment variables from .env file
+
+// const mongoUri = process.env.MONGO_URI || 'mongodb+srv://senurabawantha_db_user:koOuQbtRbIJcI4sd@bus-traking-api.hxctmjs.mongodb.net/?retryWrites=true&w=majority&appName=bus-traking-api'; // Default to local DB if not set
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(mongoUri, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true
+//     });
+//     console.log('MongoDB connected successfully');
+//   } catch (err) {
+//     console.error('Error connecting to MongoDB:', err);
+//     process.exit(1); // Exit the process if DB connection fails
+//   }
+// };
+
+// module.exports = connectDB;

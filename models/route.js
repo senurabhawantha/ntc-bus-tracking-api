7775b7da476fdb1ba4f7fbafe7b202b27a06509b
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-02 12:39:38 +0530

    Update route.js

--- models/route.js
+++ models/route.js
@@ -1,18 +1,50 @@
-// models/route.js
 const mongoose = require('mongoose');
 
 const routeSchema = new mongoose.Schema({
-  route_id: { 
-    type: Number, 
-    required: true, 
-    unique: true 
-  },
-  route_name: { 
-    type: String, 
-    required: true 
-  }
+  route_id: { type: Number, required: true, unique: true },
+  route_name: { type: String, required: true }
 });
 
 const Route = mongoose.model('Route', routeSchema);
-
 module.exports = Route;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // models/route.js
+// const mongoose = require('mongoose');
+
+// const routeSchema = new mongoose.Schema({
+//   route_id: { 
+//     type: Number, 
+//     required: true, 
+//     unique: true 
+//   },
+//   route_name: { 
+//     type: String, 
+//     required: true 
+//   }
+// });
+
+// const Route = mongoose.model('Route', routeSchema);
+
+// module.exports = Route;

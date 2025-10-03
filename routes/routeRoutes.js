76c6b87eb23f203855f666a182e1a9379acb4c53
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-03 16:31:05 +0530

    Update routeRoutes.js

--- routes/routeRoutes.js
+++ routes/routeRoutes.js
@@ -21,6 +21,30 @@ module.exports = router;
 
 
 
+
+// const express = require('express');
+// const router = express.Router();
+// const routeController = require('../controllers/routeController');
+
+// // GET all routes
+// router.get('/', routeController.getAllRoutes);
+
+// // GET details of a specific route by route_id
+// router.get('/:route_id', routeController.getRouteById);
+
+// // Optional: GET schedule for a route (if implemented)
+// router.get('/:route_id/schedule', routeController.getRouteSchedule);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
 // const express = require('express');
 // const router = express.Router();
 // const routeController = require('../controllers/routeController');

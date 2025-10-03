99bbd5dd02f139c16512b3856862e6725ac1c492
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-03 16:30:59 +0530

    Update busRoutes.js

--- routes/busRoutes.js
+++ routes/busRoutes.js
@@ -22,6 +22,30 @@ module.exports = router;
 
 
 
+// const express = require('express');
+// const router = express.Router();
+// const busController = require('../controllers/busController');
+
+// // GET all buses (optional query ?route_id=)
+// router.get('/', busController.getAllBuses);
+
+// // GET bus details by bus_id
+// router.get('/:bus_id', busController.getBusById);
+
+// // GET bus location only
+// router.get('/:bus_id/location', busController.getBusLocation);
+
+// // GET bus status only
+// router.get('/:bus_id/status', busController.getBusStatus);
+
+// module.exports = router;
+
+
+
+
+
+
+
 
 
 // const express = require('express');

2be1f8255c81d6de7eeb976b2c90fef8b1df5847
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-07 16:00:03 +0530

    Update bus.js

--- models/bus.js
+++ models/bus.js
@@ -30,21 +30,3 @@ module.exports = mongoose.models.Bus || mongoose.model('Bus', busSchema);
 
 
 
-
-
-
-
-// const mongoose = require('mongoose');
-
-// const busSchema = new mongoose.Schema({
-//   bus_id: { type: Number, unique: true },
-//   route_id: Number,
-//   status: { type: String, enum: ['On Time', 'Delayed'], default: 'On Time' },
-//   current_location: {
-//     latitude: Number,
-//     longitude: Number
-//   },
-//   last_updated: { type: Date, default: Date.now }
-// });
-
-// module.exports = mongoose.models.Bus || mongoose.model('Bus', busSchema);
